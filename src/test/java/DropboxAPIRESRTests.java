18058f7b9e573a79f63c510c2bf877b6777b5b81
dimdbr <ddd260501@gmail.com>
2021-05-08 19:20:48 +0300

    change getMetadata to getFileMetadata

--- src/test/java/DropboxAPIRESRTests.java
+++ src/test/java/DropboxAPIRESRTests.java
@@ -1,12 +1,16 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.json.simple.JSONObject;
 import org.junit.*;
+
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 import static io.restassured.config.EncoderConfig.encoderConfig;
 
@@ -44,8 +48,23 @@ public class DropboxAPIRESRTests {
                 body(requestBody.toJSONString()).
                 when().
                 post("https://api.dropboxapi.com/2/files/get_metadata");
+        JsonPath jsonPathEvaluator = response.jsonPath();
+        String id = jsonPathEvaluator.get("id");
+        System.out.println(id);
+        JSONObject requestBody2 = new JSONObject();
+        requestBody2.put("file", id);
+        requestBody2.put("actions", new ArrayList());
+        Response response2 = RestAssured.given().
+                config(RestAssured.config().
+                        encoderConfig(encoderConfig().appendDefaultContentCharsetToContentTypeIfUndefined(false))).
+                header("Authorization", "Bearer ExvWJJGZqD0AAAAAAAAAATb-W1hz4Nsbm96i32CXt5wF_AEJNsSAF88aRkZbOPYV").
+                header("Content-Type", "application/json").
+                body(requestBody2.toJSONString()).
+                when().
+                post("https://api.dropboxapi.com/2/sharing/get_file_metadata");
         System.out.println("metadata");
-        Assert.assertEquals(200, response.getStatusCode());
+        System.out.println(response2.getBody().asString());
+        Assert.assertEquals(200, response2.getStatusCode());
 
 
     }
